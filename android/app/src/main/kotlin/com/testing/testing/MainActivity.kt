1b93f9f3b629214dcf3da21a5e275645331a013a
vivaan monga <vivaan@vama.app>
2022-11-29 19:32:08 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/testing/testing/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.testing.testing
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:singular_flutter_sdk/singular.dart';
+import 'package:singular_flutter_sdk/singular_config.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+      initState() {
+              SingularConfig config = new SingularConfig('select_astro_6b529a6d', '63be08e3d7bd649ed48e1f46f83ad3aa');
+              config.customUserId = "test@test.com";
+              Singular.start(config);
+      }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+
+            ElevatedButton(
+              onPressed: () {
+                Singular.event("BlogListScreen_changeCategory");
+              },
+              child: const Text("hit singular event"),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.red, // background
+                onPrimary: Colors.yellow, // foreground
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
